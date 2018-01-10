2d6ff9705a66bed615348b9b948f4e5cc37369e7
Nikolas Barkas <Nikolaos_Barkas@hms.harvard.edu>
2018-01-10 14:24:02 -0500

    updates

--- R/pk.functions.cleaned.R
+++ R/pk.functions.cleaned.R
@@ -4,6 +4,7 @@
 #' @param apps a list of pagoda2 apps
 #' @return a list of dgCMatrices
 extractCountMatrices <- function(apps) {
+    require('parallel')
     ## Get common genes
     gl <- lapply(apps, function(r) colnames(r$counts))
     all.genes <- unique(unlist(gl))
@@ -160,5 +161,166 @@ filterComparisons <- function(comparisons, cutoff = 0.05) {
             ccc
         })
     })
+}
+
+removeNullComparisons <- function(comparisons, cutoff = 0.05) {
+    lapply(comparisons, function(cc) {
+        cc <- cc[!unlist(lapply(cc,is.null))]
+    })
+}
+
+
+
+#' Compare the cells in a cluster that spans multiple samples
+#' @param rl1 list of raw matrices 1
+#' @param rl2 list of raw matrices 2
+#' @param character vector listing the cells to compares
+#' @param n1 name of group1
+#' @param n2 names of group2
+#' @param min.cells minimum number of cells in a samples to keep it
+#' @return a DESseq2 result data.frame
+#' @export t.two.exp.comp
+t.two.exp.comp.2  <- function(rl1,rl2,cells,n1='test',n2='control',min.cells=20,pval.threshold=1e-2) {
+  ##require(DESeq2)
+  require(Matrix)
+  ## Subset apps to cells
+  rl1 <- lapply(rl1,function(x) x[rownames(x) %in% cells,,drop=F])
+  rl2 <- lapply(rl2,function(x) x[rownames(x) %in% cells,,drop=F])
+  ## Check for empty samples
+  ## Remove apps with < min.cells
+  rl1 <- rl1[unlist(lapply(rl1,nrow))>min.cells]
+  rl2 <- rl2[unlist(lapply(rl2,nrow))>min.cells]
+  ## Don't run if there are not samples in one condition after filtering
+  if (length(rl1)  == 0 | length(rl2) == 0) {
+    return(NULL)
+  }
+  cts <- do.call(cbind,lapply(c(rl1,rl2),Matrix::colSums))
+  ## Make metadata
+  coldata <- data.frame(type=rep(c(n1,n2),c(length(rl1),length(rl2))));
+  rownames(coldata) <- c(names(rl1),names(rl2))
+  ## Make DESeq2 object
+  dds <- DESeq2::DESeqDataSetFromMatrix(countData = cts,colData = coldata,design = ~ type)
+  ## Do diff expression and get results
+  dds <- DESeq2::DESeq(dds)
+  res <- DESeq2::results(dds)
+  allgenes <- rownames(res);
+  res <- as.data.frame(res);
+  res <- res[!is.na(res$pvalue),]
+  res <- res[res$pvalue<=pval.threshold,]
+  res <- res[order(res$pvalue),]
+  res$Z <- qnorm(res$pvalue)*sign(res$log2FoldChange)
+  res$Za <- qnorm(res$padj)*sign(res$log2FoldChange)
+  res <- cbind(gene=rownames(res),res)
+  if(nrow(res)>0) { # calculate mean expression values for the plots
+    cts <- t(t(cts[as.character(res$gene),])/colSums(cts)*1e6)
+    ilev <- tapply(1:ncol(cts),coldata$type,function(ii) {
+      cts[,ii,drop=F]
+    })
+    #mdf <- do.call(cbind,lapply(x$ilev,rowMeans));
+    #colnames(mdf) <- paste(colnames(mdf),'mean',sep='_')
+    #res <- cbind(res,mdf)
+  } else {
+    ilev <- NULL;
+  }
+  #res <- subset(res, pvalue < 0.05)
+  return(list(res=res,genes=allgenes,ilev=ilev,snames=c(n1,n2)))
+}
+
+
+
+#' Compare the cells in a cluster that spans multiple samples
+#' @param rl1 list of raw matrices 1
+#' @param rl2 list of raw matrices 2
+#' @param character vector listing the cells to compares
+#' @param n1 name of group1
+#' @param n2 names of group2
+#' @param min.cells minimum number of cells in a samples to keep it
+#' @param pval.threshold p-value threshold
+#' @return a DESseq2 result data.frame
+#' @export t.two.exp.comp
+t.two.exp.comp.2  <- function(rl1,rl2,cells,n1='test',n2='control',min.cells=20,pval.threshold=1e-2) {
+  ##require(DESeq2)
+  require(Matrix)
+  ## Subset apps to cells
+  rl1 <- lapply(rl1,function(x) x[rownames(x) %in% cells,,drop=F])
+  rl2 <- lapply(rl2,function(x) x[rownames(x) %in% cells,,drop=F])
+  ## Check for empty samples
+  ## Remove apps with < min.cells
+  rl1 <- rl1[unlist(lapply(rl1,nrow))>min.cells]
+  rl2 <- rl2[unlist(lapply(rl2,nrow))>min.cells]
+  ## Don't run if there are not samples in one condition after filtering
+  if (length(rl1)  == 0 | length(rl2) == 0) {
+    return(NULL)
+  }
+  cts <- do.call(cbind,lapply(c(rl1,rl2),Matrix::colSums))
+  ## Make metadata
+  coldata <- data.frame(type=rep(c(n1,n2),c(length(rl1),length(rl2))));
+  rownames(coldata) <- c(names(rl1),names(rl2))
+  ## Make DESeq2 object
+  dds <- DESeq2::DESeqDataSetFromMatrix(countData = cts,colData = coldata,design = ~ type)
+  ## Do diff expression and get results
+  dds <- DESeq2::DESeq(dds)
+  res <- DESeq2::results(dds)
+  allgenes <- rownames(res);
+  res <- as.data.frame(res);
+  res <- res[!is.na(res$pvalue),]
+  res <- res[res$pvalue<=pval.threshold,]
+  res <- res[order(res$pvalue),]
+  res$Z <- qnorm(res$pvalue)*sign(res$log2FoldChange)
+  res$Za <- qnorm(res$padj)*sign(res$log2FoldChange)
+  res <- cbind(gene=rownames(res),res)
+  if(nrow(res)>0) { # calculate mean expression values for the plots
+    cts <- t(t(cts[as.character(res$gene),])/colSums(cts)*1e6)
+    ilev <- tapply(1:ncol(cts),coldata$type,function(ii) {
+      cts[,ii,drop=F]
+    })
+    #mdf <- do.call(cbind,lapply(x$ilev,rowMeans));
+    #colnames(mdf) <- paste(colnames(mdf),'mean',sep='_')
+    #res <- cbind(res,mdf)
+  } else {
+    ilev <- NULL;
+  }
+  #res <- subset(res, pvalue < 0.05)
+  return(list(res=res,genes=allgenes,ilev=ilev,snames=c(n1,n2)))
+}
+
+
+#' Run comparisons within clusters spaning pagoda2 object boundaries
+#' @param ccm named list of raw count matrices obtained with extractCountMatrices()
+#' @param app.types a named factor of the ccm matrices with type for each
+#' @param contrasts pairs of app.types to compares
+#' @param clusters the clusters to to use (comparisons are only performed within clusters)
+#' @param mc.cores number of cores to use
+#' @return list of list of differential expression tables from DESeq2
+runAllComparisons.2 <- function(ccm, app.types, contrasts, clusters, mc.cores=16) {
+    require('nbHelpers')
+    clusters <- clusters[!is.na(clusters)] 
+    clusters.split <- factorBreakdown(clusters)
+    lapply(contrasts, function(cc) {
+        grp1 <- names(app.types)[app.types == cc[1]]
+        grp2 <- names(app.types)[app.types == cc[2]]
+        mclapply(clusters.split, function(cells.compare) {
+            tryCatch({
+                t.two.exp.comp.2(ccm[grp1],ccm[grp2],cells.compare)
+            }, error =
+                   function(e) { NULL }
+            )
+        },mc.cores =mc.cores)
+    })
+}
+
+
+#' Convert de result to json and optionally save it as a file
+#' @param xe differential expression result from t.two.exp.comp.2
+#' @param filename optional file name to save to
+#' @return json test string (invisible)
+#' @export de2json
+de2json <- function(xe, filename=NULL) {
+    xe$res <- unname(split(xe$res,1:nrow(xe$res)))
+    y <- rjson::toJSON(xe)
+    if (!is.null(filename)) {
+        write(y,file=filename)
+    }
+    invisible(y)
 }
 
