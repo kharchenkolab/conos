3605e2977cb443a55262dc3efee69646473b14d2
yarloz <lozinsky.yaroslav@gmail.com>
2018-08-24 17:37:49 -0400

    runShinyApp added

--- R/conos.r
+++ R/conos.r
@@ -492,3 +492,19 @@ greedy.modularity.cut <- function(wt,N,leaf.labels=NULL,minsize=0,minbreadth=0) 
   hc$order <- order.dendrogram(as.dendrogram(hc))
   return(list(hc=hc,leafContent=x$leafContent,deltaM=x$deltaM,breadth=as.vector(x$breadth),splits=x$splitsequence))
 }
+
+#' @export
+runShinyApp <- function(wt=NULL, N=NULL, leaf.labels=NULL, minsize=0, minbreadth=0) {
+  appDir <- system.file("app", package = "conos")
+  if (appDir == "") {
+    stop("Could not find example directory. Try re-installing `mypackage`.", call. = FALSE)
+  }
+  .GlobalEnv$.wt <- wt
+  .GlobalEnv$.N <- N
+  .GlobalEnv$.leaf.labels <- leaf.labels
+  .GlobalEnv$.minsize <- minsize
+  .GlobalEnv$.minbreadth <- minbreadth
+  .GlobalEnv$.appDir <- appDir
+  on.exit(rm(list=c(".wt", ".N", ".leaf.labels", ".minsize", ".minbreadth", ".appDir"),envir=.GlobalEnv))
+  shiny::runApp(appDir, display.mode = "normal")
+}
