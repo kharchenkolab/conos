9cca6465d6f24ea47dcecf477643d64319a334e0
viktor_petukhov <viktor.s.petuhov@ya.ru>
2018-09-28 09:04:30 -0400

    Refactor CPCA

--- src/cpca.cpp
+++ src/cpca.cpp
@@ -2,7 +2,8 @@
 // [[Rcpp::depends(RcppArmadillo)]]
 #include <RcppArmadillo.h>
 #include <iostream>
-#include <math.h>
+#include <cmath>
+
 using namespace std;
 using namespace arma;
 using namespace Rcpp;
@@ -12,7 +13,7 @@ Rcpp::List cpcaF(const arma::cube& cov,const arma::vec& ng,int ncomp=10,int maxi
   int k = cov.n_slices;
   int p = cov.n_rows;
   int n = sum(ng);
-  
+
   mat D(k,ncomp,fill::zeros);
   mat CPC(p,ncomp,fill::zeros);
   mat Qw(p,p,fill::eye);
@@ -23,52 +24,57 @@ Rcpp::List cpcaF(const arma::cube& cov,const arma::vec& ng,int ncomp=10,int maxi
   // initialization
   mat S(p,p,fill::zeros);
   for(int i=0;i<k;i++) {
-    S+=cov.slice(i) * (ng[i]/((double)n));
+    S += cov.slice(i) * (ng[i] / double(n));
   }
 
   vec eigval; mat eigvec;
   if(eigenvR.isNotNull()) {
     eigvec=as<mat>(eigenvR);
   } else {
-    eig_sym(eigval, eigvec, S);  
+    eig_sym(eigval, eigvec, S);
     // largest eigenvalues will be at teh front
     eigvec=fliplr(eigvec);
   }
-  
-  if(verbose) cout<<ncomp<<" CPCAs [";
+
+  if(verbose) Rcout << ncomp << " CPCAs [";
   for(int comp=0; comp<ncomp; comp++) {
-    //if(verbose) cout<<"comp "<<(comp+1)<<endl;
-    if(verbose) cout<<'.'<<flush;
+    //if(verbose) Rcout<<"comp "<<(comp+1)<<endl;
+    if(verbose) Rcout << '.' << flush;
+
     vec q(eigvec.col(comp));
     vec d(k,fill::zeros);
     for(int i=0;i<k;i++) {
       arma::vec v=q.t() * (cov.slice(i) * q);
       d[i]=v[0];
     }
-    //cout<<d<<endl;
+
+    //Rcout<<d<<endl;
     double cost0=0;
     int it=0;
     for(it=0; it<maxit; it++) {
       S.zeros();
       for(int i=0;i<k;i++) {
-	S+=cov.slice(i) * (ng[i]/d[i]);
+        S+=cov.slice(i) * (ng[i]/d[i]);
       }
+
       vec w=S*q;
-      
+
       if(comp!=0) {
-	w=Qw*w;
+        w=Qw*w;
       }
-      
+
       q=w/sqrt(sum(w%w));
-      
+
       for(int i=0;i<k;i++) {
-	arma::vec v=q.t() * (cov.slice(i) * q);
-	d[i]=v[0];
+        arma::vec v=q.t() * (cov.slice(i) * q);
+        // Rcout << "Sizes: " << size(cov.slice(i)) << " " << q.size() << " " << v.size() << std::endl;
+        d[i]=v[0];
       }
-       
+
       double cost=sum(log(d) % ng);
       double delta=fabs((cost-cost0)/cost);
-      //cout<<"delta["<<it<<"]="<<delta<<endl;
+
+      //Rcout<<"delta["<<it<<"]="<<delta<<endl;
       if(delta<tol) break;
       cost0 = cost;
     }
@@ -76,11 +82,11 @@ Rcpp::List cpcaF(const arma::cube& cov,const arma::vec& ng,int ncomp=10,int maxi
     D.col(comp)=d;
     CPC.col(comp)=q;
     //arma::mat v=(q * q.t());
-    //cout<<"q*t(q)="<<v<<endl;
+    //Rcout<<"q*t(q)="<<v<<endl;
     Qw-=q * q.t();
   }
-  if(verbose) cout<<"]"<<endl;
-  
+  if(verbose) Rcout<<"]"<<endl;
+
   // return
   List ret;
   ret["D"] = D.t();
